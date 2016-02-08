7ac633c358472637616918643007f95ba348b791
CyberUnit <cyberunit@gmail.com>
2016-02-08 22:06:50 +0300

    очепятка, испортил врачей в болезнях...

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -1392,7 +1392,7 @@ if (!Acc::isAuthed())
     if(count($doctors)<2) {
       $doctors_bonus = ModelManagerFactory::getByName('doctor')->getListByDoctorSearchParams($doctor_search_params);
       for($i=0; $i<(2 - count($doctors)); $i++) {
-        $doctors[] = $doctors[$i];
+        $doctors[] = $doctors_bonus[$i];
       }
     }
     $doctor_search_params->ids_no = [];

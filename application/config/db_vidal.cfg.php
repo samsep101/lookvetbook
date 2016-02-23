6858bd05f7d22db145e6bc8e36388eef5aa9cfa2
CyberUnit <cyberunit@gmail.com>
2016-02-23 22:44:39 +0300

    config change

--- application/config/db_vidal.cfg.php
+++ application/config/db_vidal.cfg.php
@@ -1,11 +1,14 @@
 <?php
-	// data base config
-	define('DB_VIDAL_PREFIX','');
-	define('DB_VIDAL_HOST', 'localhost');
-	define('DB_VIDAL_NAME', 'vidal');
-	define('DB_VIDAL_USER', 'root');
-	define('DB_VIDAL_PASSWORD', 'gfhjkm');
-	define('DB_VIDAL_INIT','SET NAMES `utf8`');
+// data base config
+define('DB_VIDAL_PREFIX', '');
+define('DB_VIDAL_HOST', 'localhost');
+//define('DB_VIDAL_NAME', 'vidal');
+//define('DB_VIDAL_USER', 'root');
+//define('DB_VIDAL_PASSWORD', 'gfhjkm');
+define('DB_VIDAL_NAME', 'lookmedb_vidal');
+define('DB_VIDAL_USER', 'lookmedb_vidal');
+define('DB_VIDAL_PASSWORD', 'vidal123qa');
+define('DB_VIDAL_INIT', 'SET NAMES `utf8`');
 
-	Application::addClassDir(Application::getApplicationDir() . '/models/vidal', 'Manager', 'manager');
-	Application::addClassDir(Application::getApplicationDir() . '/models/vidal', 'Model', 'model');
+Application::addClassDir(Application::getApplicationDir() . '/models/vidal', 'Manager', 'manager');
+Application::addClassDir(Application::getApplicationDir() . '/models/vidal', 'Model', 'model');

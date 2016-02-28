ab2e091eef4a1247e2d286431ab6c32660c913e0
CyberUnit <cyberunit@gmail.com>
2016-02-29 00:06:35 +0300

    для того, чтобы завести lookvetbook

--- application/controllers/admin/security.php
+++ application/controllers/admin/security.php
@@ -1,91 +1,92 @@
 <?php
-    class SecurityAdminController  extends Controller {
 
-        public $layout = 'admin';
+class SecurityAdminController extends Controller
+{
 
-        //авторизация пользователя
-        function login() {
-            if ($this->request->isPost())
-            {
-                $login = $this->request('login');
-                $password = $this->request('password');
+  public $layout = 'admin';
 
-                $user_manager = new UserManager();
+  //авторизация пользователя
+  function login()
+  {
+    if ($this->request->isPost()) {
+      $login = $this->request('login');
+      $password = $this->request('password');
 
-                $user = $user_manager->getOneByLoginAndPassword($login, sha1($password));
+      $user_manager = new UserManager();
 
-                if ($user)
-                {
-                    $grant_manager = new GrantManager();
-                    $user_grants = $grant_manager->getListByRoleId($user->role_id);
-                    Acl::saveAuthData($user,$user_grants);
+      $user = $user_manager->getOneByLoginAndPassword($login, sha1($password));
 
-                    $admin_role = array(
-						RoleModel::ACCOUNT_ADMIN,
-						RoleModel::CALL_CENTRE_OPERATOR,
-						RoleModel::ESHOP_MANAGER,
-						RoleModel::ESHOP_CONTENT_MANAGER
-					);
+      if ($user) {
+        $grant_manager = new GrantManager();
+        $user_grants = $grant_manager->getListByRoleId($user->role_id);
+        Acl::saveAuthData($user, $user_grants);
 
-                    if (in_array(Acl::userRole(), $admin_role))
-                        $this->redirectUrl(ADMIN_FOLDER);
-                    elseif (Acl::isAuthed(RoleModel::ACCOUNT_SUPER_MANAGER)
-						|| Acl::isAuthed(RoleModel::ACCOUNT_MANAGER)
-						|| Acl::isAuthed(RoleModel::FREELANCE_MANAGER)
-					)
-                        $this->redirectUrl(MANAGE_FOLDER);
-                    elseif (Acl::isAuthed(RoleModel::ACCOUNT_REGISTRY))
-                        $this->redirectUrl(REGISTRY_FOLDER);
-                }
+        $admin_role = array(
+          RoleModel::ACCOUNT_ADMIN,
+          RoleModel::CALL_CENTRE_OPERATOR,
+          RoleModel::ESHOP_MANAGER,
+          RoleModel::ESHOP_CONTENT_MANAGER
+        );
 
-                $this->view->msg = 'Неверные эл. почта или пароль.';
-                $this->view->login = $login;
-                $this->view->pass = $password;
-            }
+        if (in_array(Acl::userRole(), $admin_role))
+          $this->redirectUrl(ADMIN_FOLDER);
+        elseif (Acl::isAuthed(RoleModel::ACCOUNT_SUPER_MANAGER)
+          || Acl::isAuthed(RoleModel::ACCOUNT_MANAGER)
+          || Acl::isAuthed(RoleModel::FREELANCE_MANAGER)
+        )
+          $this->redirectUrl(MANAGE_FOLDER);
+        elseif (Acl::isAuthed(RoleModel::ACCOUNT_REGISTRY))
+          $this->redirectUrl(REGISTRY_FOLDER);
+      }
 
-            AdminAuthHelper::redirectToDefaultPage();
-
-        }
-
-        //
-        function hashKeylogin()
-        {
-            Acl::logout();
-            $onlyWorkAccess = (int) SettingsManager::get('work_access');
-            if($onlyWorkAccess){
-                $hashKey = $this->request('hashKey','');
-                if($hashKey){
-                    $_SESSION['hashKey'] = $hashKey;
-                }
-            }
-            $this->redirect('security','login');
-        }
-
-        //завершение работы пользователем
-        function logout()
-        {
-            setcookie('city_id', 0, time() + 60 * 60 * 24 * 365, '/');
-
-            if(Acl::userLogin()){
-                $logger = new Logger(array(),'user_logout',Acl::userLogin(),array());
-                $logger->send();
-            }
-            Acl::logout();
-
-            $this->redirectUrl(ADMIN_FOLDER.'/security/login');
-        }
-
-        //при отказе в доступе
-        function denied()
-        {
-            $this->view->setLayout('simple');
-            $this->render('admin/security/denied');
-        }
-
-        //
-        public function error404()
-        {
-            $this->view->setLayout('simple');
-            $this->render('error404');
-        }
+      $this->view->msg = 'Неверные эл. почта или пароль.';
+      $this->view->login = $login;
+      $this->view->pass = $password;
     }
+
+    AdminAuthHelper::redirectToDefaultPage();
+
+  }
+
+  //
+  function hashKeylogin()
+  {
+    Acl::logout();
+    $onlyWorkAccess = (int)SettingsManager::get('work_access');
+    if ($onlyWorkAccess) {
+      $hashKey = $this->request('hashKey', '');
+      if ($hashKey) {
+        $_SESSION['hashKey'] = $hashKey;
+      }
+    }
+    $this->redirect('security', 'login');
+  }
+
+  //завершение работы пользователем
+  function logout()
+  {
+    setcookie('city_id', 0, time() + 60 * 60 * 24 * 365, '/');
+
+    if (Acl::userLogin()) {
+      $logger = new Logger(array(), 'user_logout', Acl::userLogin(), array());
+      $logger->send();
+    }
+    Acl::logout();
+
+    $this->redirectUrl(ADMIN_FOLDER . '/security/login');
+  }
+
+  //при отказе в доступе
+  function denied()
+  {
+    $this->view->setLayout('simple');
+    $this->render('admin/security/denied');
+  }
+
+  //
+  public function error404()
+  {
+    $this->view->setLayout('simple');
+    $this->render('error404');
+  }
+}
--- core/classes/controller.class.php
+++ core/classes/controller.class.php
@@ -1,112 +1,113 @@
 <?php
-    /**
-     * Parent class for all application controllers
-     */
-    class Controller
-    {
-        public $view;
-        public $request;
-        public $action;
-        public $controller;
-        public $layout = NULL;
 
-        protected $folder;
-        private $isRendered = FALSE;
+/**
+ * Parent class for all application controllers
+ */
+class Controller
+{
+  public $view;
+  public $request;
+  public $action;
+  public $controller;
+  public $layout = NULL;
 
-        public function __construct()
-        {
-            $this->request = new Request();
-        }
+  protected $folder;
+  private $isRendered = FALSE;
 
-        public function setFolder($folder)
-        {
-            $this->folder = $folder;
-        }
+  public function __construct()
+  {
+    $this->request = new Request();
+  }
 
-        /**
-         * Render template
-         *
-         * @param string $templateName template name, for example "mytpl","myfolder/mytpl"
-         */
-        public function render($templateName = '')
-        {
-            $this->beforeRender();
-            // you could set layout by $this->view->setLayout();
-            if (!empty($this->layout))
-                $this->view->setLayout($this->layout);
-            $templatePath = $this->getTemplatePath($templateName);
+  public function setFolder($folder)
+  {
+    $this->folder = $folder;
+  }
 
-            $this->view->render($templatePath);
+  /**
+   * Render template
+   *
+   * @param string $templateName template name, for example "mytpl","myfolder/mytpl"
+   */
+  public function render($templateName = '')
+  {
+    $this->beforeRender();
+    // you could set layout by $this->view->setLayout();
+    if (!empty($this->layout))
+      $this->view->setLayout($this->layout);
+    $templatePath = $this->getTemplatePath($templateName);
 
-            $this->isRendered = TRUE;
-        }
+    $this->view->render($templatePath);
 
-        public function renderInString($templateName)
-        {
-            ob_start();
-            $this->beforeRender();
-            // you could set layout by $this->view->setLayout();
-            if (!empty($this->layout))
-                $this->view->setLayout($this->layout);
-            $templatePath = $this->getTemplatePath($templateName);
-            $this->view->render($templatePath);
+    $this->isRendered = TRUE;
+  }
 
-            $content = ob_get_contents();
-	          ob_end_clean();
+  public function renderInString($templateName)
+  {
+    ob_start();
+    $this->beforeRender();
+    // you could set layout by $this->view->setLayout();
+    if (!empty($this->layout))
+      $this->view->setLayout($this->layout);
+    $templatePath = $this->getTemplatePath($templateName);
+    $this->view->render($templatePath);
 
-            return $content;
-        }
+    $content = ob_get_contents();
+    ob_end_clean();
 
-        public function request($name, $defaulValue = NULL)
-        {
-            return $this->request->getParam($name, $defaulValue);
-        }
+    return $content;
+  }
 
-        /**
-         * Is any template already has been rendered by current action ?
-         * @return bool is any template already has been rendered by current action
-         */
-        public function isRendered()
-        {
-            return $this->isRendered;
-        }
+  public function request($name, $defaulValue = NULL)
+  {
+    return $this->request->getParam($name, $defaulValue);
+  }
 
-        /**
-         * Redirect to custom URL
-         *
-         * @param string $url URL
-         */
-        public function redirectUrl($url = '/')
-        {
-            RedirectManager::redirect($url);
-			exit();
-        }
+  /**
+   * Is any template already has been rendered by current action ?
+   * @return bool is any template already has been rendered by current action
+   */
+  public function isRendered()
+  {
+    return $this->isRendered;
+  }
 
-        /**
-         * Redirect to URL specifed by controller, action and params
-         *
-         * @param string $action
-         * @param string $controller
-         * @param string $params
-         */
-        public function redirect($action = 'index', $controller = 'index', $params = '')
-        {
-            if ($params)
-                $params = '?' . $params;
+  /**
+   * Redirect to custom URL
+   *
+   * @param string $url URL
+   */
+  public function redirectUrl($url = '/')
+  {
+    RedirectManager::redirect($url);
+    exit();
+  }
 
-            header('location: ' . Application::getHttpRoot() . $controller . '/' . $action . '/' . $params);
-            die();
-        }
+  /**
+   * Redirect to URL specifed by controller, action and params
+   *
+   * @param string $action
+   * @param string $controller
+   * @param string $params
+   */
+  public function redirect($action = 'index', $controller = 'index', $params = '')
+  {
+    if ($params)
+      $params = '?' . $params;
 
-        /**
-         * Error 404 posting
-         *
-         */
-        public function error404($exception = NULL)
-        {
-            if (debug) {
-                echo '<html><head><style>';
-                echo '
+    header('location: ' . Application::getHttpRoot() . $controller . '/' . $action . '/' . $params);
+    die();
+  }
+
+  /**
+   * Error 404 posting
+   *
+   */
+  public function error404($exception = NULL)
+  {
+    if (debug) {
+      echo '<html><head><style>';
+      echo '
 	* {
 		font-family: Verdana, Tahoma, Arial;
 		font-size: 13px;
@@ -147,50 +148,49 @@
 	}
 	
 	';
-                echo '</style></head></body>';
-                echo '<center><div class="error"><h1>' . $exception->getMessage() . '</h1>';
-                foreach ($exception->getTrace() as $key=> $item) {
-                    echo '<div class="error-item"><div class="error-num">' . ($key + 1) . '.</div>';
-                    echo '<div class="error-descr">';
-                    echo '<span>' . $item['class'] . ' ' . $item['type'] . ' ' . $item['function'] . ' ' . '( ' . join(', ', $item['args']) . ' )' . '</span><br>';
-                    echo $item['file'] . ' (Line: ' . $item['line'] . ')<br>';
-                    echo '</div><br clear="all" /></div>';
-                }
-                echo '</div></center>';
-                echo '</body></html>';
-            } else {
-                header("location: http://" . $_SERVER['SERVER_NAME'] . "/error404");
-            }
-            die();
-        }
-
-
-        public function beforeRender()
-        {
-        }
-
-        /**
-         * Get path to template by template name
-         *
-         * @param string $templateName
-         *
-         * @return string template path
-         */
-        private function getTemplatePath($templateName)
-        {
-
-            $folder = ($this->folder) ? $this->folder . '/' : '';
-
-            $path = NULL;
-            if (preg_match('/\//', $templateName)) {
-                $path = Application::getTemplatesDir(TRUE) . '/' . $templateName;
-                ;
-            } elseif (strlen($templateName) != 0) {
-                $path = Application::getTemplatesDir(TRUE) . '/' . $this->controller . '/' . $templateName;
-            } else {
-                $path = Application::getTemplatesDir(TRUE) . '/' . $folder . $this->controller . '/' . $this->action;
-            }
-
-            return $path;
-        }
+      echo '</style></head></body>';
+      echo '<center><div class="error"><h1>' . $exception->getMessage() . '</h1>';
+      foreach ($exception->getTrace() as $key => $item) {
+        echo '<div class="error-item"><div class="error-num">' . ($key + 1) . '.</div>';
+        echo '<div class="error-descr">';
+        echo '<span>' . $item['class'] . ' ' . $item['type'] . ' ' . $item['function'] . ' ' . '( ' . join(', ', $item['args']) . ' )' . '</span><br>';
+        echo $item['file'] . ' (Line: ' . $item['line'] . ')<br>';
+        echo '</div><br clear="all" /></div>';
+      }
+      echo '</div></center>';
+      echo '</body></html>';
+    } else {
+      header("location: http://" . $_SERVER['SERVER_NAME'] . "/error404");
     }
+    die();
+  }
+
+
+  public function beforeRender()
+  {
+  }
+
+  /**
+   * Get path to template by template name
+   *
+   * @param string $templateName
+   *
+   * @return string template path
+   */
+  private function getTemplatePath($templateName)
+  {
+
+    $folder = ($this->folder) ? $this->folder . '/' : '';
+
+    $path = NULL;
+    if (preg_match('/\//', $templateName)) {
+      $path = Application::getTemplatesDir(TRUE) . '/' . $templateName;
+    } elseif (strlen($templateName) != 0) {
+      $path = Application::getTemplatesDir(TRUE) . '/' . $this->controller . '/' . $templateName;
+    } else {
+      $path = Application::getTemplatesDir(TRUE) . '/' . $folder . $this->controller . '/' . $this->action;
+    }
+
+    return $path;
+  }
+}

5f5585e32b212b2497662c43ac6b30ed69caa583
CyberUnit <cyberunit@gmail.com>
2016-03-09 23:23:13 +0300

    правки, связанные с закачкой файлов изображений

--- core/classes/types/category.type.php
+++ core/classes/types/category.type.php
@@ -1,88 +1,89 @@
 <?php
 
-	class CategoryType extends Type
-	{
+class CategoryType extends Type
+{
 
-		public function getFormValue($val = '', $model = false) {
-		  $valid = $val;
+  public function getFormValue($val = '', $model = false)
+  {
+    $valid = $val;
 
-			$sort_param = isset($this->fieldInfo['sort_by'])?$this->fieldInfo['sort_by']:'';
-			$params = [];
+    $sort_param = isset($this->fieldInfo['sort_by']) ? $this->fieldInfo['sort_by'] : '';
+    $params = [];
 
-		  if (isset($this->fieldInfo['where'])) {
-			  $params[] = [$this->fieldInfo['where']['param'], $this->fieldInfo['where']['value']];
-		  }
+    if (isset($this->fieldInfo['where'])) {
+      $params[] = [$this->fieldInfo['where']['param'], $this->fieldInfo['where']['value']];
+    }
 
-		  if (isset($this->fieldInfo['search_params']) && $model) {
-				foreach ($this->fieldInfo['search_params'] as $param => $field_name){
-					if ($param == 'join'){
-					  $params[] = ['join', $field_name];
-				  } else {
-					  if($field_name) {
-						  if(is_array($field_name)) {
-							  $params[] = [$field_name['field_name'], $field_name['value']];
-						  } else {
-							  $params[] = [$param, $model->{$field_name}];
-						  }
-					  } else {
-						  $params[] = [$param, $model->{$field_name}];
-					  }
-				  }
-			  }
-		  }
+    if (isset($this->fieldInfo['search_params']) && $model) {
+      foreach ($this->fieldInfo['search_params'] as $param => $field_name) {
+        if ($param == 'join') {
+          $params[] = ['join', $field_name];
+        } else {
+          if ($field_name) {
+            if (is_array($field_name)) {
+              $params[] = [$field_name['field_name'], $field_name['value']];
+            } else {
+              $params[] = [$param, $model->{$field_name}];
+            }
+          } else {
+            $params[] = [$param, $model->{$field_name}];
+          }
+        }
+      }
+    }
 
-			$style = isset($this->fieldInfo['style']) ? $this->fieldInfo['style'] : 'width: 50%';
+    $style = isset($this->fieldInfo['style']) ? $this->fieldInfo['style'] : 'width: 50%';
 
-			$tag_class = 'form__' . $this->getFieldName();
+    $tag_class = 'form__' . $this->getFieldName();
 
-			$result = '<div class="cross_name '.$tag_class.'" style="display: none;">'.json_encode($this->fieldInfo['cross_name']).'</div>';
-			$result .= '<div class="cross_table '.$tag_class.'" style="display: none;">'.json_encode($this->fieldInfo['cross_table']).'</div>';
-			$result .= '<div class="sort_param '.$tag_class.'" style="display: none;">'.json_encode($sort_param).'</div>';
-			$result .= '<div class="search_param '.$tag_class.'" style="display: none;">'.json_encode($param).'</div>';
-			$result .= '<div class="value '.$tag_class.'" style="display: none;">'.str_replace('<','%$%',$val).'</div>';
-
-
-			if (isset($this->fieldInfo['short'])) {
-			  $result .= '<select class="filling_select '.$tag_class.'" name="form[' . $this->getFieldName() . ']" style="'.$style.'" >';
-
-			  if (!empty($this->fieldInfo['first'])) {
-				  foreach ($this->fieldInfo['first'] as $key=> $value) {
-					  if ($value == $this->value)
-						  $selected = 'selected';
-					  elseif ($value == $valid)
-						  $selected = 'selected'; else
-						  $selected = '';
-					  $result .= '<option value="' . $key . '" ' . $selected . ' class="first">' . htmlspecialchars($value) . '</option>';
-				  }
-			  }
-			  $result .= "</select>";
-			}else {
-				$result .= '<input class="filling_input ' . $tag_class . '" style="' . $style . '" />';
-				$result .= '<div class="filling_select ' . $tag_class . '" style="' . $style . '; display: none;" ></div>';
-				$result .= '<input class="filling_value ' . $tag_class . '" name="form[' . $this->getFieldName() . ']" style="display: none;" value="' . htmlspecialchars($val) . '" />';
-			}
-
-		  if (isset($this->fieldInfo['script']))
-		  {
-			  $result .= '<script>'.$this->fieldInfo['script'].'</script>';
-		  }
-		  return $result;
-		}
+    $result = '<div class="cross_name ' . $tag_class . '" style="display: none;">' . json_encode($this->fieldInfo['cross_name']) . '</div>';
+    $result .= '<div class="cross_table ' . $tag_class . '" style="display: none;">' . json_encode($this->fieldInfo['cross_table']) . '</div>';
+    $result .= '<div class="sort_param ' . $tag_class . '" style="display: none;">' . json_encode($sort_param) . '</div>';
+    $result .= '<div class="search_param ' . $tag_class . '" style="display: none;">' . json_encode($params) . '</div>';
+    $result .= '<div class="value ' . $tag_class . '" style="display: none;">' . str_replace('<', '%$%', $val) . '</div>';
 
 
+    if (isset($this->fieldInfo['short'])) {
+      $result .= '<select class="filling_select ' . $tag_class . '" name="form[' . $this->getFieldName() . ']" style="' . $style . '" >';
 
-		public function getViewValue($value)
-		{
-			$model = ModelManagerFactory::getByName($this->fieldInfo['cross_table'])->getOneById($value);
-			if (!$model) {
-				if (isset($this->fieldInfo['if_null']))
-					return $this->fieldInfo['if_null'];
-				else
-					return NULL;
-			}
+      if (!empty($this->fieldInfo['first'])) {
+        foreach ($this->fieldInfo['first'] as $key => $value) {
+          if ($value == $this->value){
+            $selected = 'selected';
+          }elseif ($value == $valid) {
+              $selected = 'selected';
+          }else{
+              $selected = '';
+          }
+          $result .= '<option value="' . $key . '" ' . $selected . ' class="first">' . htmlspecialchars($value) . '</option>';
+        }
+      }
+      $result .= "</select>";
+    } else {
+      $result .= '<input class="filling_input ' . $tag_class . '" style="' . $style . '" />';
+      $result .= '<div class="filling_select ' . $tag_class . '" style="' . $style . '; display: none;" ></div>';
+      $result .= '<input class="filling_value ' . $tag_class . '" name="form[' . $this->getFieldName() . ']" style="display: none;" value="' . htmlspecialchars($val) . '" />';
+    }
 
-			$field = $this->fieldInfo['cross_name'];
-			return '<a href="' . ADMIN_FOLDER . '/' . $this->fieldInfo['cross_table'] . '/edit?id=' . $model->getId() . '" target="_blank">' . $model->$field . '</a>';
-		}
+    if (isset($this->fieldInfo['script'])) {
+      $result .= '<script>' . $this->fieldInfo['script'] . '</script>';
+    }
+    return $result;
+  }
 
-	}
+
+  public function getViewValue($value)
+  {
+    $model = ModelManagerFactory::getByName($this->fieldInfo['cross_table'])->getOneById($value);
+    if (!$model) {
+      if (isset($this->fieldInfo['if_null']))
+        return $this->fieldInfo['if_null'];
+      else
+        return NULL;
+    }
+
+    $field = $this->fieldInfo['cross_name'];
+    return '<a href="' . ADMIN_FOLDER . '/' . $this->fieldInfo['cross_table'] . '/edit?id=' . $model->getId() . '" target="_blank">' . $model->$field . '</a>';
+  }
+
+}
--- core/classes/view.class.php
+++ core/classes/view.class.php
@@ -1,118 +1,118 @@
 <?php
 
-    class View extends Dynamic
-    {
+class View extends Dynamic
+{
 
-        public $sufix = 'ViewHelper';
+  public $sufix = 'ViewHelper';
 
-        private $__values;
+  private $__values;
 
-        private $__extension = '.tpl';
+  private $__extension = '.tpl';
 
-        private $__layout;
+  private $__layout;
 
-        private $__template;
+  private $__template;
 
-        /**
-         * @var ViewFilter[]
-         */
-        private $view_filters = array();
+  /**
+   * @var ViewFilter[]
+   */
+  private $view_filters = array();
 
-        public function addViewFilter(ViewFilter $filter)
-        {
-            $this->view_filters[] = $filter;
-        }
+  public function addViewFilter(ViewFilter $filter)
+  {
+    $this->view_filters[] = $filter;
+  }
 
-        public function __construct()
-        {
-            $this->__values = array();
-        }
+  public function __construct()
+  {
+    $this->__values = array();
+  }
 
-        public function __set($varName, $value)
-        {
-            $this->__values[$varName] = $value;
-        }
+  public function __set($varName, $value)
+  {
+    $this->__values[$varName] = $value;
+  }
 
-        public function __get($varName)
-        {
-            if (!empty($this->__values[$varName]))
-                return $this->__values[$varName];
-            else
-                return NULL;
-        }
+  public function __get($varName)
+  {
+    if (!empty($this->__values[$varName]))
+      return $this->__values[$varName];
+    else
+      return NULL;
+  }
 
-        public function render($templateName)
-        {
-            ob_start();
-            extract($this->__values);
-            $this->__template = $templateName . $this->__extension;
-            if (is_file($this->__layout)) {
-	            include($this->__layout);
-            } else {
-	            include($this->__template);
-            }
-            $html = ob_get_contents();
-            //ob_clean();
-	          ob_end_clean();
-
-            echo $this->filter($html);
-        }
-
-		public function renderInString($templateName)
-		{
-			ob_start();
-			extract($this->__values);
-			$this->__template = Application::getTemplatesDir(TRUE) . '/'. $templateName . $this->__extension;
-			include($this->__template);
-			$html = ob_get_contents();
-			ob_end_clean();
-
-			return $this->filter($html);
-		}
-
-        public function block($templateName, $params = null)
-        {
-            if (debug == 1)
-                echo "<!--".$templateName."-->";
-
-            extract($this->__values);
-            if ($params)
-                extract($params);
-            $file = Application::getTemplatesDir(TRUE) . '/' . $templateName . $this->__extension;
-            include($file);
-        }
-
-        public function content()
-        {
-            extract($this->__values);
-            include($this->__template);
-        }
-
-        public function clear()
-        {
-            $this->__values = array();
-        }
-
-        public function setLayout($templatePath)
-        {
-            $this->__layout = Application::getTemplatesDir(TRUE) . '/' . $templatePath . $this->__extension;
-        }
-
-        public function getLayout()
-        {
-            return $this->__layout;
-        }
-
-        protected function filter($html)
-        {
-            if($this->view_filters)
-            {
-                foreach($this->view_filters as $filter)
-                {
-                    $html = $filter->filter($html);
-                }
-            }
-
-            return $html;
-        }
+  public function render($templateName)
+  {
+    ob_start();
+    extract($this->__values);
+    $this->__template = $templateName . $this->__extension;
+    if (is_file($this->__layout)) {
+      include($this->__layout);
+    } else {
+      include($this->__template);
     }
+    $html = ob_get_contents();
+    //ob_clean();
+    ob_end_clean();
+
+    echo $this->filter($html);
+  }
+
+  public function renderInString($templateName)
+  {
+    ob_start();
+    extract($this->__values);
+    $this->__template = Application::getTemplatesDir(TRUE) . '/' . $templateName . $this->__extension;
+    include($this->__template);
+    $html = ob_get_contents();
+    ob_end_clean();
+
+    return $this->filter($html);
+  }
+
+  public function block($templateName, $params = null)
+  {
+    if (debug == 1)
+      echo "<!--" . $templateName . "-->";
+
+    extract($this->__values);
+    if ($params)
+      extract($params);
+    $file = Application::getTemplatesDir(TRUE) . '/' . $templateName . $this->__extension;
+    include($file);
+  }
+
+  public function content()
+  {
+    extract($this->__values);
+    if(file_exists($this->__template)) {
+      include($this->__template);
+    }
+  }
+
+  public function clear()
+  {
+    $this->__values = array();
+  }
+
+  public function setLayout($templatePath)
+  {
+    $this->__layout = Application::getTemplatesDir(TRUE) . '/' . $templatePath . $this->__extension;
+  }
+
+  public function getLayout()
+  {
+    return $this->__layout;
+  }
+
+  protected function filter($html)
+  {
+    if ($this->view_filters) {
+      foreach ($this->view_filters as $filter) {
+        $html = $filter->filter($html);
+      }
+    }
+
+    return $html;
+  }
+}

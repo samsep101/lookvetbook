ea7e4cfc9055e65ab77907b3dba4f8356c752636
CyberUnit <cyberunit@gmail.com>
2016-03-13 22:50:36 +0300

    no header

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 ﻿<?php
-header("Content-Type: text/html; charset=UTF-8");
+if (php_sapi_name()!='cli') {
+  header("Content-Type: text/html; charset=UTF-8");
+}
 define('debug', 0);
 
 if (!debug) {

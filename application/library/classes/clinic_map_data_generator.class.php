c63680d75d264f275f18a05f11dcd03712c24863
CyberUnit <cyberunit@gmail.com>
2016-03-13 23:19:15 +0300

    бага старых разработчиков

--- application/library/classes/clinic_map_data_generator.class.php
+++ application/library/classes/clinic_map_data_generator.class.php
@@ -1,39 +1,40 @@
 <?php
-    class ClinicMapDataGenerator
-    {
-        public function generate(ClinicSearchParams $clinic_search_params)
-        {
-            $clinic_search_params = clone $clinic_search_params;
-            $clinic_search_params->page = null;
-            $clinic_search_params->by_page = null;
 
-            $hash = $clinic_search_params->getParamsHash();
+class ClinicMapDataGenerator
+{
+  public function generate(ClinicSearchParams $clinic_search_params)
+  {
+    $clinic_search_params = clone $clinic_search_params;
+    $clinic_search_params->page = null;
+    $clinic_search_params->by_page = null;
 
-            if (!file_exists('/media/map/' . $hash . '.js')) {
-                /**
-                 * @var ClinicManager $clinic_manager
-                 */
-                $clinic_manager = ModelManagerFactory::getByName('clinic');
-                $clinics = $clinic_manager->getListByClinicSearchParams($clinic_search_params);
+    $hash = $clinic_search_params->getParamsHash();
 
-                $str = '';
+    if (!file_exists('./media/map/' . $hash . '.js')) {
+      /**
+       * @var ClinicManager $clinic_manager
+       */
+      $clinic_manager = ModelManagerFactory::getByName('clinic');
+      $clinics = $clinic_manager->getListByClinicSearchParams($clinic_search_params);
 
-                /**
-                 * @var ClinicModel[] $clinics
-                 */
-                if ($clinics)
-                    foreach ($clinics as $clinic) {
-                        $str .= $clinic->getId() . ':' .
-                            $clinic->address . ':' . $clinic->name . ':' .
-                            $clinic->latitude . ':' . $clinic->longitude . ':4|';
-                    }
+      $str = '';
 
-
-                $str = trim($str, '|');
-
-                file_put_contents('./media/map/' . $hash . '.js', $str);
-            }
-
-            return $hash;
+      /**
+       * @var ClinicModel[] $clinics
+       */
+      if ($clinics)
+        foreach ($clinics as $clinic) {
+          $str .= $clinic->getId() . ':' .
+            $clinic->address . ':' . $clinic->name . ':' .
+            $clinic->latitude . ':' . $clinic->longitude . ':4|';
         }
+
+
+      $str = trim($str, '|');
+
+      file_put_contents('./media/map/' . $hash . '.js', $str);
     }
+
+    return $hash;
+  }
+}

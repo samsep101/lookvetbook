bd6934c36021aa4686ae11b51a47e4c68b782f9d
Server Operator <cyber.un.it@gmail.com>
2016-03-24 23:24:58 +0300

    ajax doctor search - if bonus doctors not found

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -1416,7 +1416,7 @@ if (!Acc::isAuthed())
 
     if(count($doctors)<2) {
       $doctors_bonus = ModelManagerFactory::getByName('doctor')->getListByDoctorSearchParams($doctor_search_params);
-      for($i=0; $i<=(2 - count($doctors)); $i++) {
+      for($i=0; $i<=(2 - count($doctors)) and $i<count($doctors_bonus); $i++) {
         $doctors[] = $doctors_bonus[$i];
       }
     }

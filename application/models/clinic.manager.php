2fb43db1d48c85eeb23a708696e4e6b251a9edbd
CyberUnit <cyberunit@gmail.com>
2016-06-27 01:29:00 +0300

    debug clinic search

--- application/models/clinic.manager.php
+++ application/models/clinic.manager.php
@@ -552,6 +552,7 @@ class ClinicManager extends AliasManager
     }
     $this->total_hits = $clinic_index_manager->getTotalHits();
 
+    return $result;
     return $this->getListByIds($result);
     //устарело
 /*

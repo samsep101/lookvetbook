126fa2f0e5d9e8ee2d7f3e6b794b92a87d2e51d2
anharchenko <hghsasha@gmail.com>
2016-07-13 10:19:01 +0300

    video integration

--- application/controllers/disease.php
+++ application/controllers/disease.php
@@ -159,7 +159,14 @@ class DiseaseController extends BaseController
     $dis_param = $this->request('dis');
     $this->view->disease_green_btn = ($dis_param && $dis_param == 'new3') ? 1 : 0;
     $this->view->actions = (new ActionManager())->getListForDisease($disease->getId());
-	$this->view->actions = array($this->view->actions[array_rand($this->view->actions)]);
+    if ($this->view->actions)
+	    $this->view->actions = array($this->view->actions[array_rand($this->view->actions)]);
+
+      $videos = ['1','2','3'];
+      $video = $videos[array_rand($videos)];
+      $this->view->video_file_path = '/media/images/vids/'.$video.'.mp4';
+      $this->view->video_thumb_path = '/media/images/vids/'.$video.'.jpg';
+
 
 
       if (in_array($disease->alias,['pnevmoniya'])){

06ee9b94a413659ba80632df4a791e451f072268
anharchenko <hghsasha@gmail.com>
2016-09-16 15:18:20 +0300

    fixes

--- application/library/classes/elasticsearch/index_controls/elastic_search_clinic_index_control.class.php
+++ application/library/classes/elasticsearch/index_controls/elastic_search_clinic_index_control.class.php
@@ -158,7 +158,7 @@ class ElasticSearchClinicIndexControl extends ElasticSearchModelIndexControl
              'lon' => $criteria->geo_point->getLongitude()
          );
          $distance = (($criteria->distance)/10000).'km';
-         $distance = '50km';
+         $distance = '3km';
          $match = new \Elastica\Filter\GeoDistance('geo_point', $location, $distance);
          $filter_and->addFilter($match);
      }

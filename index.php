66bd49995debe7ca9a48e1716fbfc843bab4d484
Server user <admin@lookmedbook.ru>
2017-04-11 00:01:59 +0300

    sentry - запуск объекта

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ if (!debug) {
   //xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 }
 
+//$client = new Raven_Client('https://3eddb6b698414aa28519bd1b864ef789:92e01d140f5c448c81348fd7834e201c@sentry.io/157050');
+
 try {
   if (!empty($argc)) {
     chdir(dirname(__FILE__));

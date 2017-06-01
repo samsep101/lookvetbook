eeadff1196780151ab341e15c1c473f93c0c01aa
Sergey Byakov <gunslover@gmail.com>
2017-06-01 17:25:49 +0300

    fix

--- application/library/helpers/seo_link_view.helper.php
+++ application/library/helpers/seo_link_view.helper.php
@@ -84,7 +84,7 @@ class SeoLinkViewHelper
     if ($_SERVER['REQUEST_URI']=='/disease/gripp' or $_SERVER['REQUEST_URI']=='/disease/diareya') {
         $linkOuter=str_replace('<a rel="nofollow" class="jsLinkHidingIndexing" href="" data-link="https://docdoc.ru" target="_blank"','<a class="jsLinkHidingIndexing" href="https://docdoc.ru" data-link="https://docdoc.ru" target="_blank"',$linkOuter);
         $linkOuter=str_replace('<a rel="nofollow" class="jsLinkHidingIndexing" href="" data-link="https://spb.docdoc.ru" target="_blank"','<a class="jsLinkHidingIndexing" href="https://spb.docdoc.ru" data-link="https://spb.docdoc.ru" target="_blank"',$linkOuter);
-        $linkOuter=str_replace('<a rel="nofollow" class="jsLinkHidingIndexing" href="https://docdoc.ru/doctor/gastroenterolog" target="_blank"','<a class="jsLinkHidingIndexing" href="https://docdoc.ru/doctor/gastroenterolog" target="_blank"',$linkOuter);
+        $linkOuter=str_replace('<a rel="nofollow" class="jsLinkHidingIndexing" href="" data-link="https://docdoc.ru/doctor/gastroenterolog" target="_blank"','<a class="jsLinkHidingIndexing" href="https://docdoc.ru/doctor/gastroenterolog" data-link="https://docdoc.ru/doctor/gastroenterolog" target="_blank"',$linkOuter);
         
     }
     return $linkOuter;

0493410999905f78a5a42ccffbeb99fc4317630e
Server user <admin@lookmedbook.ru>
2017-06-05 09:52:26 +0300

    fucking warnings must died

--- application/controllers/clinic.php
+++ application/controllers/clinic.php
@@ -62,11 +62,11 @@ class ClinicController extends BaseController
             for ($j=0;$j<=count($v);++$j) {
                 $r[]=$v[$i].'-'.$v[$j];
                 for ($k=0;$k<=count($v);++$k) {
-                    $r[]=$v[$i].'-'.$v[$j].'-'.$v[$k];
+                    $r[]=(isset($v[$i]) and isset($v[$j]) and isset($v[$k])) ? $v[$i].'-'.$v[$j].'-'.$v[$k] : '';
                     for ($z=0;$z<=count($v);++$z) {
-                        $r[]=$v[$i].'-'.$v[$j].'-'.$v[$k].'-'.$v[$z];
+                        $r[]=(isset($v[$i]) and isset($v[$j]) and isset($v[$k]) and isset($v[$z])) ? $v[$i].'-'.$v[$j].'-'.$v[$k].'-'.$v[$z] : '';
                         for ($q=0;$q<=count($v);++$q) {
-                            $r[]=$v[$i].'-'.$v[$j].'-'.$v[$k].'-'.$v[$z].'-'.$v[$q];
+                            $r[]= (isset($v[$i]) and isset($v[$j]) and isset($v[$k]) and isset($v[$z]) and isset($v[$q]))  ? $v[$i].'-'.$v[$j].'-'.$v[$k].'-'.$v[$z].'-'.$v[$q] : '';
                         }
                     }
                 }

a8ddef357eb7727ee00f2956c2828663bd2329b0
Playmore <playmoredevelop@gmail.com>
2017-06-30 17:30:12 +0300

    2574 Разместить статью Фосфоглив

--- application/controllers/shop/product.php
+++ application/controllers/shop/product.php
@@ -1,6 +1,11 @@
 <?php
 	class ProductShopController extends BaseController
 	{
+        /**
+         * @var ProductModel $product
+         */
+        private $_product = false;
+
 		public function get()
 		{
             $this->view->menu_active = 'shop';
@@ -9,39 +14,38 @@
 
             /**
              * @var ProductManager $product_manager
-             * @var ProductModel $product
              * @var ProductCategoryModel $product_category
              * @var ProductCategoryModel $parent_product_category
              */
             $product_manager = ModelManagerFactory::getByName('product');
 
-            $product = $product_manager->getOneByIdOrAlias($product_id);
+            $this->_product = $product_manager->getOneByIdOrAlias($product_id);
 
             if(!isset($this->current_account) || !$this->current_account || !$this->current_account->is_product_admin)
             {
-                if(!$product || !$product->is_active)
+                if(!$this->_product || !$this->_product->is_active)
                 {
                     ErrorPageViewHelper::page404();
                 }
             }
             else
             {
-                if(!$product)
+                if(!$this->_product)
                 {
                     ErrorPageViewHelper::page404();
                 }
             }
 
-            if (is_numeric($product_id) && $product->alias)
+            if (is_numeric($product_id) && $this->_product->alias)
             {
-                RedirectManager::redirect301(ProductLinkViewHelper::getLink($product));
+                RedirectManager::redirect301(ProductLinkViewHelper::getLink($this->_product));
             }
 
-            $product_category = $product->product_category;
+            $product_category = $this->_product->product_category;
             $parent_product_category = null;
 
             if ($product_category) {
-                $this->view->category_id = $product->product_category->getId();
+                $this->view->category_id = $this->_product->product_category->getId();
                 $this->view->product_category = $product_category;
 
                 $parent_product_category = $product_category->parent;
@@ -52,11 +56,35 @@
                 }
             }
 
-            $this->view->product = $product;
+            $this->view->product = $this->_product;
             $this->view->product_category = $product_category;
             $this->view->parent_product_category = $parent_product_category;
-            $this->view->page_title = ProductsPageTitleGeneratorHelper::productTitle($product);
+            $this->view->page_title = ProductsPageTitleGeneratorHelper::productTitle($this->_product);
+
+            $this->_getBeforeArticle();
 
             $this->render('shop/product/index');
 		}
+
+        /**
+         * Шаблон статьи перед текстом инструкции, выводим если есть
+         */
+        protected function _getBeforeArticle() {
+
+            $alias = $this->_product->alias;
+
+            // проверяем шаблон для отображения перед текстом инструкции
+            // если он есть - подключаем его
+            $path = implode('/', [
+                Application::getTemplatesDir(true),
+                'shop',
+                'product',
+                'articles',
+                $alias . $this->view->getExtension()
+            ]);
+            if(file_exists($path)){
+                $this->view->before_article = $this->view->renderInString('shop/product/articles/'.$alias, false);
+            }
+        }
+
 	}

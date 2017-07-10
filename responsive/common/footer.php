c8b80870db71a9b5dfb76e1bca73ce4831c395cc
Playmore <playmoredevelop@gmail.com>
2017-07-10 13:39:32 +0300

    10.07.2017
    backup

---
+++ responsive/common/footer.php
@@ -0,0 +1,95 @@
+    <div class="container-fluid">
+        <div class="row">
+            <noindex>
+                <footer class="footer">
+                    <div class="container">
+                        <div class="footer-inner-top ff-bold">
+                            <section class="inner-top-info"> Нужна помощь?
+                                <span class="help-phone">8 495 215 09 07</span> или
+                                <a class="info-mail jsLinkHidingIndexing" href="mailto:help@lookmedbook.ru">help@lookmedbook.ru</a>
+                            </section>
+                            <ul class="inner-top-socials">
+                                <li class="socials-vkontakte"><a class="jsLinkHidingIndexing" target="_blank" href="http://vk.com/lookmedbook"></a></li>
+                                <li class="socials-odnoklassniki"><a class="jsLinkHidingIndexing" target="_blank" href="http://odnoklassniki.ru/group/52035885072448"></a></li>
+                                <li class="socials-facebook"><a class="jsLinkHidingIndexing" target="_blank" href="http://www.facebook.com/LookMedBook"></a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-inner-bottom">
+                            <section class="inner-bottom-copyright ff-bold">© «lookmedbook.citrus.one», 2017</section>
+                            <a class="reg-link show_license" href="javascript:void(0);">Пользовательское соглашение</a>
+                            <ul class="inner-bottom-navigation ff-bold">
+                                <li><a class="help-link jsLinkHidingIndexing" href="/help">Помощь</a></li>
+                                <li><a class="about-link jsLinkHidingIndexing" href="/about">О проекте</a></li>
+                                <li><a class="about-link jsLinkHidingIndexing" href="/smap">Карта сайта</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </footer>
+            </noindex>
+        </div>
+    </div>
+
+    <link rel="stylesheet" href="/media/css/chosen.css?0.1" type="text/css"/>
+    <link rel="stylesheet" href="/media/css/jquery.fancybox.css?0.1" type="text/css"/>
+    <link rel="stylesheet" href="/media/css/jquery.jscrollpane.css?0.1" type="text/css"/>
+    <link rel="stylesheet" href="/media/css/jquery-ui.min.css?0.1" type="text/css"/>
+    <link rel="stylesheet" href="/media/js/jquery-rating/styles/jquery.rating.css?0.1" type="text/css"/>
+    <link rel="stylesheet" href="/media/css/look/styles-widget.css?0.1" type="text/css"/>
+
+
+    <!--[if lte IE 9]>
+    <script src="/media/js/jquery.placeholder.min.js?0.1"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="/media/js/js_core.js?0.1"></script>
+
+    <!--[if lt IE 10]>
+    <script type="text/javascript" src="/media/js/flashcanvas.js?0.1"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="/media/js/js_library.js?43"></script>
+
+    <script type="text/javascript" src="/media/js/jquery-ui-1.10.2.custom.min.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery.maskedinput.min.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery.carouFredSel-6.2.0-packed.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/chosen.jquery.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery.maskedinput-1.3.min.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/bootstrap-affix.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/waypoints.min.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery.fancybox.pack.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery.jscrollpane.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery.jcarousel.min.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jcarousel.connected-carousels.js?0.1"></script>
+    <script type="text/javascript" src="/js/validation"></script>
+    <script type="text/javascript" src="/media/js/jquery.raty.min.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/cookies.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/linkHidingIndexing.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/jquery-rating/js/jquery.rating-2.0.js?0.1"></script>
+    <script src="/media/js/jquery.event.move.js?0.1"></script>
+
+
+
+
+    <script src="http://api-maps.yandex.ru/2.0/?load=package.full,package.clusters,package.overlays&lang=ru-RU" type="text/javascript"></script>
+    <script src="/media/js/geolacation-button.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/magazine_total_price.js?0.1"></script>
+    <script type="text/javascript" src="/media/js/image_preview.js?0.1"></script>
+
+    <script type="text/javascript" src="/media/js/responsive-switch.js"></script>
+    <script src="https://docdoc.ru/widget/js" type="text/javascript"></script>
+
+    <script async type="text/javascript" src="//sjsmartcontent.org/static/plugin-site/js/sjplugin.js" site="6fmj"></script>
+
+    <script>
+        $(document).ready(function () {
+            window.city_controller = new CityController(2, 55.7558, 37.6176);
+        });
+    </script>
+
+    <script type="text/javascript">
+        SessionInfo.domain = "lookmedbook.citrus.one";
+        SessionInfo.csrf = 'r5SB86hDK4hrDh7SHN6AHBBAZrb8FYRYQziKrya3';
+    </script>
+
+</body>
+</html>
---
+++ responsive/common/header.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="description" content="Ищете врача  в Москве? LookMedBook поможет выбрать опытного врача по отзывам и рейтингам клиентов, узнать стоимость и записаться на прием.">
+        <meta name="yandex-verification" content='76535cc7dd5d586f' />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Врачи в Москве - запись на прием, цены, отзывы и рейтинги на LookMedBook</title>
+
+        <link rel="icon" href="/media/images/home_page/look/favicon.png" type="image/png">
+        <link rel="canonical" href="http://lookmedbook.citrus.one//doctor" />
+
+        <link rel="stylesheet" href="/media/responsive/styles.min.css?0.1" type="text/css" />
+
+        <!--[if lt IE 9]>
+        <link rel="stylesheet" type="text/css" media="screen,projection" href="/media/css/for_ie.css?0.1"/>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js?0.1"></script>
+        <![endif]-->
+        <!--[if lte IE 9]>
+        <link rel="stylesheet" type="text/css" media="screen,projection" href="/media/css/for_ie9.css?0.1"/>
+        <![endif]-->
+        <!--[if lte IE 8]>
+        <link rel="stylesheet" type="text/css" media="screen,projection" href="/media/css/for_ie8.css?0.1"/>
+        <![endif]-->
+        <!--[if IE 11]>
+        <link rel="stylesheet" type="text/css" media="screen,projection" href="/media/css/for_ie11.css?0.1"/>
+        <![endif]-->
+
+        <script type="text/javascript" src="/media/js/jquery-1.8.3.min.js?0.1"></script>
+
+        <script type="text/javascript" src="/media/js/inputmask/jquery.inputmask.js?0.1"></script>
+        <script type="text/javascript" src="/media/js/inputmask/jquery.inputmask.numeric.extensions.js?0.1"></script>
+        <script type="text/javascript" src="/media/js/inputmask/jquery.inputmask.date.extensions.js?0.1"></script>
+
+    </head>
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+                <header class="header">
+                    <div class="container">
+                        <div class="flo">
+
+                            <a class="logo" href="/" title="Портал медицинских услуг в Москве – LookMedBook">
+                                <img class="main-logo" src="/media/images/blank.png" alt="Портал медицинских услуг в Москве – LookMedBook">
+                            </a>
+
+                            <nav>
+                                <a href="#" class="toggle-opener"><i class="glyphicon glyphicon-menu-hamburger"></i></a>
+                                <ul class="nav-table">
+                                    <li><a class="active doctor-link" href="/doctor">Врачи</a></li>
+                                    <li><a class=" clinic-link" href="/clinic">Клиники</a></li>
+                                    <li><a class=" disease-link" href="/disease">Заболевания</a></li>
+                                    <li class="treatment-in-switz"><a class="" href="/shop/catalog">Лекарства</a></li>
+                                    <li class="treatment-in-switz"><a class="" href="/action">Акции</a></li>
+                                </ul>
+                            </nav>
+
+                            <form action="/disease/searchResults" method="GET">
+                                <div class="quick-search" id="disease-quick-search">
+                                    <div class="fields">
+                                        <input type="text" class="quick-search-input" autocomplete="off" name="disease_query" placeholder="Найти заболевание">
+                                        <input type="submit" class="quick-search-submit" data-action-for-counters="top" value="">
+                                    </div>
+                                    <ul class="drop-menu" style="">
+
+                                    </ul>
+                                </div>
+                            </form>
+
+                            <div id="authorization-block-on-disease-page" class="no-auth-buttons">
+                                <a class="btn-enter reg-linking ff-bold" href="javascript:void(0);">Войти</a>
+                                <a class="btn-reg reg-linking ff-bold" data-action-for-counters="top-reg" href="javascript:void(0)">Зарегистрироваться</a>
+                            </div>
+
+                        </div>
+
+                        <div class="clearfix"></div>
+                    </div>
+                </header>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    <div class="fake-box" ></div>
+
+                </div>
+                <div class="clearfix"></div>
+                
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div class="fake-box" bg-red></div>
+                </div>
+                <div class="col-lg-8 col-md-6 col-sm-6 col-xs-12">
+                    <div class="fake-box" ></div>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                     <div class="fake-box" ></div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="fake-box" bg-red></div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                     <div class="fake-box" style="min-height: 150px"></div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="fake-box" bg-red></div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    <div class="fake-box" style="min-height: 300px"></div>
+
+                </div>
+                <div class="clearfix"></div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div class="fake-box" bg-red></div>
+                </div>
+                <div class="col-lg-8 col-md-6 col-sm-6 col-xs-12">
+                    <div class="fake-box" ></div>
+                </div>
+
+            </div>
+
+        </div>

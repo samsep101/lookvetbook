e8c89dfa0efaa055a890125b0ba95671bbcc5007
Unknown <belokon.pv@gmail.com>
2017-08-02 10:47:48 +0300

    2685:

--- _getout.php
+++ _getout.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,7 +8,7 @@
 $domain_td = "http://wwwapteka.info/";
 
 if (!empty($_GET['g'])) {
-echo file_get_contents($domain_td. '/output/index/' . $_GET['g']);
+   echo file_get_contents($domain_td. '/output/index/' . $_GET['g']);
 }
 
 ?>

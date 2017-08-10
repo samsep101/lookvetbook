6be319d448f2c92c2738005bda0097492bb41d58
Unknown <belokon.pv@gmail.com>
2017-08-10 11:23:09 +0300

    Add deleted Doctors

--- application/controllers/import.php
+++ application/controllers/import.php
@@ -29,22 +29,22 @@ class ImportController extends BaseController
         $update = 0;
         $notfaund = 0;
         $clinics = $this->_getClinics();
-        //$clinics = $this->_getClinics([1496, 1497]);
+        //$clinics = $this->_getClinics([249, 1497]);
         $clinic_data_url = 'https://lookmedbook:IzkmbB@back.docdoc.ru/api/rest/1.0.6/json/clinic/';
         $doctor_data_url = 'https://lookmedbook:IzkmbB@back.docdoc.ru/api/rest/1.0.6/json/doctor/';
         foreach ($clinics as $clinic){
 #if ($clinic->id != 3819)continue;
 
-            echo "clinic: $clinic->name<br>".PHP_EOL;
-            try{
+        echo "clinic: $clinic->name<br>".PHP_EOL;
+        try{
 		echo $clinic_data_url.$clinic->docdoc_id.PHP_EOL;
-                $s = file_get_contents($clinic_data_url.$clinic->docdoc_id);
-                $data = json_decode($s);
-                //var_dump($data); exit();
-                $data = $data->Clinic[0];
-            }catch (Exception $exp){
-                continue;
-            }
+            $s = file_get_contents($clinic_data_url.$clinic->docdoc_id);
+            $data = json_decode($s);
+            //var_dump($data); exit();
+            $data = $data->Clinic[0];
+        }catch (Exception $exp){
+            continue;
+        }
 
             // импорт адреса
             if(!empty($data->Street) AND !empty($data->House) AND !empty($data->StreetId)){
@@ -55,7 +55,6 @@ class ImportController extends BaseController
                 $clinic->latitude = $data->Latitude;
                 $clinic->longitude = $data->Longitude;
             }
-
             $clinic->about = $data->Description;
 
             $clinicId = $clinic->id;
@@ -70,12 +69,10 @@ class ImportController extends BaseController
 
             foreach( $metro as $m ){
 
-
                 $metroBranch  = ( new MetroBranchManager() )->getListByIds( array($m->LineId) );
                 if( !empty ( $metroBranch ) ){
 
                      $metroStation = ( new MetroStationManager() )->getListByName($m->Name);
-
 
                     if( !empty( $metroStation ) ){
 
@@ -107,9 +104,6 @@ class ImportController extends BaseController
 
             }
 
-
-
-
             $clinic_specialty = [];
 
             $q = "delete from doctor_to_clinic where clinic_id='$clinic->id'";
@@ -128,118 +122,128 @@ class ImportController extends BaseController
                 }catch (Exception $exp){
                     continue;
                 }
+
+                list($last_name, $first_name, $second_name) = explode(' ', $docdata->Name);
 //                if ($docdata->Slots)
 //                    pr($docdata->Slots, 1);
 
-				list($last_name, $first_name, $second_name) = explode(' ', $docdata->Name);
+                # Checking if the doctor was deleted
+                $deletedDoctor =  ( new DeletedDoctorManager() )->getListByQuery("
+                    SELECT * FROM deleted_doctor
+                    WHERE last_name LIKE \"{$last_name}\"
+                    AND first_name LIKE \"{$first_name}\"
+                    AND second_name LIKE \"$second_name\" ");
 
-                $q = "select doctor.id
-                      from doctor
-                      where
-					  doctor.last_name like '$last_name' and
-					  doctor.first_name like '$first_name' and
-					  doctor.second_name like '$second_name' ";
+                if( empty( $deletedDoctor) ){
 
-                if ($a = $db->query($q)){
-                    $doctor = (new DoctorManager())->getOneById($a[0]['id']);
-                    echo "doctor: $doctor->full_lower_name ($doctor->id)<br>".PHP_EOL;
-                }else{
-                    $doctor = new DoctorModel();
-                    echo "new doctor: $docdata->Name<br>".PHP_EOL;
-                }
+                    $q = "select doctor.id
+                          from doctor
+                          where
+                          doctor.last_name like '$last_name' and
+                          doctor.first_name like '$first_name' and
+                          doctor.second_name like '$second_name' ";
 
-                $doctor->last_name = $last_name;
-				$doctor->first_name = $first_name;
-				$doctor->second_name = $second_name;
-                $price = $docdata->Price > 0 ? (float) $docdata->Price : '';
+                    if ($a = $db->query($q)){
+                        $doctor = (new DoctorManager())->getOneById($a[0]['id']);
+                        echo "doctor: $doctor->full_lower_name ($doctor->id)<br>".PHP_EOL;
+                    }else{
+                        $doctor = new DoctorModel();
+                        echo "new doctor: $docdata->Name<br>".PHP_EOL;
+                    }
 
-                $doctor->full_lower_name = strtolower($docdata->Name);
-                $doctor->sex_id = ($docdata->Sex == 1) ? 2 : 1;
-                $doctor->rate = (float) $docdata->Rating;
-                $doctor->work_experience = 0;
-                $doctor->advice_rate = (float) $docdata->Rating;
-                $doctor->cabinet_rate = (float) $docdata->Rating;
-                $doctor->relationship_rate = (float) $docdata->Rating;
-                $doctor->value_for_money_rate = (float) $docdata->Rating;
-                $doctor->diagnosis_is_clear_rate = (float) $docdata->Rating;
-                $doctor->about = $docdata->Description;
-                $doctor->is_active = 1;
-                $doctor->availability = 1;
-                $doctor->is_has_morning_time = 1;
-                $doctor->is_has_evening_time = 1;
-                $doctor->is_has_weekend_time = 1;
-                $doctor->is_leave_the_house = 1;
-                $doctor->is_children = 1;
-                $doctor->is_adult = 1;
-                $doctor->is_pregnant = 1;
-                $doctor->is_handicapped = 1;
-                $doctor->save();
+                    $doctor->last_name = $last_name;
+                    $doctor->first_name = $first_name;
+                    $doctor->second_name = $second_name;
+                    $price = $docdata->Price > 0 ? (float) $docdata->Price : '';
 
-				$q = "select doctor.id
-                      from doctor
-                      where
-					  doctor.last_name like '$last_name' and
-					  doctor.first_name like '$first_name' and
-					  doctor.second_name like '$second_name' ";
+                    $doctor->full_lower_name = strtolower($docdata->Name);
+                    $doctor->sex_id = ($docdata->Sex == 1) ? 2 : 1;
+                    $doctor->rate = (float) $docdata->Rating;
+                    $doctor->work_experience = 0;
+                    $doctor->advice_rate = (float) $docdata->Rating;
+                    $doctor->cabinet_rate = (float) $docdata->Rating;
+                    $doctor->relationship_rate = (float) $docdata->Rating;
+                    $doctor->value_for_money_rate = (float) $docdata->Rating;
+                    $doctor->diagnosis_is_clear_rate = (float) $docdata->Rating;
+                    $doctor->about = $docdata->Description;
+                    $doctor->is_active = 1;
+                    $doctor->availability = 1;
+                    $doctor->is_has_morning_time = 1;
+                    $doctor->is_has_evening_time = 1;
+                    $doctor->is_has_weekend_time = 1;
+                    $doctor->is_leave_the_house = 1;
+                    $doctor->is_children = 1;
+                    $doctor->is_adult = 1;
+                    $doctor->is_pregnant = 1;
+                    $doctor->is_handicapped = 1;
+                    $doctor->save();
 
-                if ($a = $db->query($q)){
-                    $doctor = (new DoctorManager())->getOneById($a[0]['id']);
-                }else{
-                    echo "error doc add: $docdata->Name<br>".PHP_EOL;
-					continue;
-                }
+                    $q = "select doctor.id
+                          from doctor
+                          where
+                          doctor.last_name like '$last_name' and
+                          doctor.first_name like '$first_name' and
+                          doctor.second_name like '$second_name' ";
 
-                foreach ($docdata->Specialities as $specialty){
-
-                    $t = (new SpecialtyManager())->getOneByAliasOrSyninim($specialty->Alias);
-                    if (!$t){
-                        echo "<span style='color:red'>specialty $specialty->Name ($specialty->Alias) not found</span><br>".PHP_EOL;
+                    if ($a = $db->query($q)){
+                        $doctor = (new DoctorManager())->getOneById($a[0]['id']);
+                    }else{
+                        echo "error doc add: $docdata->Name<br>".PHP_EOL;
                         continue;
                     }
 
-                    $specialty = $t;
-                    $clinic_specialty[$specialty->id] = $specialty;
+                    foreach ($docdata->Specialities as $specialty){
+
+                        $t = (new SpecialtyManager())->getOneByAliasOrSyninim($specialty->Alias);
+                        if (!$t){
+                            echo "<span style='color:red'>specialty $specialty->Name ($specialty->Alias) not found</span><br>".PHP_EOL;
+                            continue;
+                        }
+
+                        $specialty = $t;
+                        $clinic_specialty[$specialty->id] = $specialty;
 
 
-                    $q = "select id from doctor_to_clinic where clinic_id='$clinic->id' and doctor_id='$doctor->id' and specialty_id='$specialty->id'";
-                    if (!$db->query($q)){
-                        $q = "insert into doctor_to_clinic set clinic_id='$clinic->id', doctor_id='$doctor->id', specialty_id='$specialty->id'". ($price ? " , first_visit_price='$price'" : '');
+                        $q = "select id from doctor_to_clinic where clinic_id='$clinic->id' and doctor_id='$doctor->id' and specialty_id='$specialty->id'";
+                        if (!$db->query($q)){
+                            $q = "insert into doctor_to_clinic set clinic_id='$clinic->id', doctor_id='$doctor->id', specialty_id='$specialty->id'". ($price ? " , first_visit_price='$price'" : '');
+                            $db->query($q);
+                        }
+
+                        $q = "insert into doctor_specialty_to_clinic set clinic_id='$clinic->id', doctor_id='$doctor->id', specialty_id='$specialty->id'";
                         $db->query($q);
                     }
 
-					$q = "insert into doctor_specialty_to_clinic set clinic_id='$clinic->id', doctor_id='$doctor->id', specialty_id='$specialty->id'";
-                    $db->query($q);
-                }
-
-                if ($docdata->Img){
-                    $tmp_name = ABS_ROOT.'/media/upload/clinic/license/tmp_doctor_'.$doctor->id.'.jpg';
-					echo "tmp file name:".$tmp_name."<br>".PHP_EOL;
-                    if (file_exists($tmp_name))
-                    {
-                        unlink($tmp_name);
-                    }
-
-                    // продолжаем процесс только если картинка сохранена
-                    if(false !== file_put_contents($tmp_name, file_get_contents($docdata->Img))){
-
-                        chmod($tmp_name , 0755);
-
-                        $image_id = ImageUploader::upload(['upload_folder' => 'clinic/license/'], ['name' => 'doctor_'.$doctor->id.'.jpg', 'tmp_name' => $tmp_name], 'doctor_'.$doctor->id);
-
-                        $doctor->image_id = $image_id;
-                        $doctor->card_image_id = $image_id;
-                        $doctor->save();
-
+                    if ($docdata->Img){
+                        $tmp_name = ABS_ROOT.'/media/upload/clinic/license/tmp_doctor_'.$doctor->id.'.jpg';
+                        echo "tmp file name:".$tmp_name."<br>".PHP_EOL;
                         if (file_exists($tmp_name))
                         {
                             unlink($tmp_name);
                         }
 
-                        $q = "delete from image_to_doctor where doctor_id=$doctor->id";
-                        $db->query($q);
+                        // продолжаем процесс только если картинка сохранена
+                        if(false !== file_put_contents($tmp_name, file_get_contents($docdata->Img))){
 
-                        $q = "insert into image_to_doctor set doctor_id=$doctor->id, image_id=$image_id";
-                        $db->query($q);
+                            chmod($tmp_name , 0755);
+
+                            $image_id = ImageUploader::upload(['upload_folder' => 'clinic/license/'], ['name' => 'doctor_'.$doctor->id.'.jpg', 'tmp_name' => $tmp_name], 'doctor_'.$doctor->id);
+
+                            $doctor->image_id = $image_id;
+                            $doctor->card_image_id = $image_id;
+                            $doctor->save();
+
+                            if (file_exists($tmp_name))
+                            {
+                                unlink($tmp_name);
+                            }
+
+                            $q = "delete from image_to_doctor where doctor_id=$doctor->id";
+                            $db->query($q);
+
+                            $q = "insert into image_to_doctor set doctor_id=$doctor->id, image_id=$image_id";
+                            $db->query($q);
+                        }
                     }
                 }
             }
--- application/models/deleted_doctor.model.php
+++ application/models/deleted_doctor.model.php
@@ -2,6 +2,8 @@
 	/**
 	 * @property int $id
 	 * @property int $clinic_id
+	 * @property int $look_id
+	 * @property int $docdoc_id
 	 * @property ClinicModel $clinic
 	 * @property string $first_name
 	 * @property string $second_name

fea1ee541f153997114136b81c6fe13292f7461e
Pavel Gorshkov <pavel.gorshkov@mpaas.res21.net>
2017-10-30 17:49:57 +0300

    #1926 Оптимизирован запрос для получения списка улиц smap

--- application/models/street.manager.php
+++ application/models/street.manager.php
@@ -46,14 +46,12 @@
 		{
 			$sql = 'SELECT *
 					FROM street s
-					WHERE EXISTS (
-							SELECT *
+					WHERE id IN (
+							SELECT DISTINCT c.street_id
 							FROM doctor dc
 							INNER JOIN doctor_to_clinic d2c ON d2c.doctor_id = dc.id
 							INNER JOIN clinic c ON c.id = d2c.clinic_id
-							WHERE
-								c.street_id = s.id
-								AND dc.is_active = 1
+							WHERE dc.is_active = 1
 						)
 					ORDER BY `name`';
 			$data = $this->db->query($sql);
@@ -68,15 +66,14 @@
             if($city_id) {
 			$sql = 'SELECT *
 					FROM street s
-					WHERE EXISTS (
-							SELECT *
+					WHERE id IN (
+							SELECT DISTINCT c.street_id
 							FROM doctor dc
 							INNER JOIN doctor_to_clinic d2c ON d2c.doctor_id = dc.id
 							INNER JOIN clinic c ON c.id = d2c.clinic_id
 							WHERE
-								c.street_id = s.id
-								AND dc.is_active = 1
-                                                                AND c.city_id = '.$city_id.'
+								dc.is_active = 1
+                                AND c.city_id = '.$city_id.'
 						)
 					ORDER BY `name`';
                 

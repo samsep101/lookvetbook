96ecfe3b930b22aab9635b2c8e2c3cd68d3a419c
Timofey Isakov <tiso@breakingbrain.org>
2017-11-16 13:44:06 +0300

    Поправлен билд js-а

--- application/library/helpers/extended_remote_compiler.helper.php
+++ application/library/helpers/extended_remote_compiler.helper.php
@@ -5,4 +5,18 @@ class ExtendedRemoteCompilerHelper extends \Closure\RemoteCompiler
     protected $url = 'https://closure-compiler.appspot.com/compile';
 
     protected $port = 443;
+
+    public function getRequestHandler()
+    {
+        if (!isset($this->requestHandler)) {
+            $requestHandler = new \Zend\Http\Client();
+            $requestHandler->setOptions(array(
+                'timeout'=> 60,
+                'adapter' => 'Zend\Http\Client\Adapter\Curl'
+            ));
+            $this->setRequestHandler($requestHandler);
+        }
+
+        return $this->requestHandler;
+    }
 }

eb4f02e6aa58555a959bd118d6b9adf2284eef69
root <root@cs26848.lookmedbook.ru>
2026-01-28 10:42:01 +0300

    upload media

---
+++ application/config/elasticsearch.cfg.php
@@ -0,0 +1,26 @@
+<?php
+
+	Application::addClassDir(Application::getApplicationDir() . '/library/classes/elasticsearch/classes', '', 'class');
+	Application::addClassDir(Application::getApplicationDir() . '/library/classes/elasticsearch/formatters', '', 'class');
+	Application::addClassDir(Application::getApplicationDir() . '/library/classes/elasticsearch/interfaces', '', 'interface');
+	Application::addClassDir(Application::getApplicationDir() . '/library/classes/elasticsearch/mappings', '', 'class');
+	Application::addClassDir(Application::getApplicationDir() . '/library/classes/elasticsearch/index_controls', '', 'class');
+	Application::addClassDir(Application::getApplicationDir() . '/library/classes/elasticsearch/index_commands', '', 'class');
+
+	Register::add('ELASTIC_SEARCH_INDEX', 'lookvetbook');
+
+        Register::add('ELASTICA_SERVERS', array(
+                "servers" => array(array(
+                        'host' => 'localhost',
+                        'port' => 9400
+                ))
+        ));
+
+	$index_models = array(
+		'ProductModel',
+		'DiseaseModel',
+		'ClinicModel',
+		'DoctorModel'
+	);
+
+	Register::add('index_models', $index_models);
---
+++ application/config/site.cfg.php
@@ -0,0 +1,37 @@
+<?php
+define('debug', 0);
+
+define('CONTACT_EMAIL','');
+define('OUTPUT_LANGUAGE','RUSSIAN');
+define('USE_SET_NAMES',1);
+define('HTTP_ROOT','');
+define('MEDIA_UPLOAD_PATH', '/media/upload/');
+define('UPLOAD_IMAGES_WIDTH', 1000);
+define('UPLOAD_IMAGES_HEIGHT', 800);
+define('ADMIN_FOLDER', '/admin');
+define('MANAGE_FOLDER', '/registry/manage');
+define('REGISTRY_FOLDER', '/registry');
+if (isset($_SERVER['HTTP_X_SCHEME']) && $_SERVER['HTTP_X_SCHEME'] == 'https') {
+    define('SITE_SCHEME', 'https');
+} elseif (php_sapi_name() === 'cli') {
+    define('SITE_SCHEME', 'https');
+} else {
+    define('SITE_SCHEME', 'http');
+}
+define('SITE_URL', SITE_SCHEME . "://lookvetbook.ru");
+define('SITE_DOMAIN', "LookVetBook.ru");
+define('SITE_NAME', "LookVetBook");
+define('SHOP_ENABLE', 1);
+define('RELEASE_NUMBER', 43);
+define('SITE_PHONE_CODE', '495');
+define('SITE_PHONE', '215-09-07');
+define('CSS_DIR', 'vet');
+define('PAGE_TITLE', 'Портал ветеринарных услуг в ');
+define('JUR_ADDRESS', 'Гамсоновский переулок, 2');
+define('JUR_ADDRESS_FULL', '115191, г.Москва, Гамсоновский переулок, д.2, офис 200');
+define('CONTENT_DISEASE_URL', 'http://admin:Eghfd24Kzq@content.'.SITE_DOMAIN.'/media/xml/Test.xml');
+
+define('DOCTOR', 'ветеринар');
+define('DOCTORA', 'ветеринара');
+
+define('MEDICYNY', 'ветеринарии');
